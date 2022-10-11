43c9e3696cb69ec8276ec8060717b96e57f3e57d
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2022-10-11 17:10:45 +0200

    Rewritting Java Test Class in Php

--- src/main/php/Twitter.php
+++ src/main/php/Twitter.php
@@ -8,7 +8,7 @@ require 'IObservable.php';
 
 class Twitter implements IObservable
 {
-    public function __construct(){}
+    public function __construct(array $observers = null){}
 
     public function subscribe(array $observers):void
     {
@@ -26,6 +26,11 @@ class Twitter implements IObservable
     }
 
     public function getObservers():array
+    {
+        throw new RuntimeException();
+    }
+
+    public function getTwits():array
     {
         throw new RuntimeException();
     }
--- src/test/php/testTwitter.php
+++ src/test/php/testTwitter.php
@@ -1,13 +1,12 @@
 <?php
 
 namespace TestSocialNetwork;
+use JetBrains\PhpStorm\Pure;
 use PHPUnit\Framework\TestCase;
+use SocialNetwork\EmptyListOfSubscribersException;
+use SocialNetwork\Follower;
+use SocialNetwork\SubscriberAlreadyExistsException;
 use SocialNetwork\Twitter;
-
-require '..\..\main\php\Twitter.php';
-
-# Require for Visual studio Code
-# require 'src\main\php\Twitter.php';
 
 class testTwitter extends TestCase
 {
@@ -20,7 +19,7 @@ class testTwitter extends TestCase
         $this->twitter = new Twitter();
     }
 
-    public function test_Observers_AfterInstantiationWithoutObservers_Success()
+    public function test_getObservers_AfterInstantiationWithoutObservers_Success()
     {
         //given
         //refer to Setup method
@@ -30,24 +29,139 @@ class testTwitter extends TestCase
         //event is called directly by the assertion
 
         //then
-        $this->assertEquals($expectedAmountOfObservers, Count($this->twitter->getObservers()));
+        $this->assertCount($expectedAmountOfObservers, $this->twitter->getObservers());
     }
 
-    public function test_Observers_AfterInstantiationWithObservers_Success()
+    public function test_getObservers_AfterInstantiationWithObservers_Success()
     {
         //given
         //refer to Setup method
         $expectedAmountOfObservers = 10;
+        $this->twitter($this->generateObservers($expectedAmountOfObservers));
 
         //when
-        $this->twitter->subscribe($this->generateObservers($expectedAmountOfObservers));
+        //event is called directly by the assertion
 
         //then
-        $this->assertEquals($expectedAmountOfObservers, Count($this->twitter->getObservers()));
+        $this->assertCount($expectedAmountOfObservers, $this->twitter->getObservers());
+    }
+
+    public function test_twits_AfterInstantiation_Success(){
+        //given
+        //refer to Before Each method
+        $expectedAmountOfTwits = 0;
+
+        //when
+        //event is called directly by the assertion
+
+        //then
+        $this->assertCount($expectedAmountOfTwits, $this->twitter->getTwits());
+    }
+
+    public function test_notifyObservers_EmptyListOfSubscriber_ThrowsException(){
+        //given
+        //refer to Before Each method
+
+        //when
+        //event is called directly by the assertion
+
+        //then
+        $this->expectException(EmptyListOfSubscribersException::class);
+        $this->twitter->notifyObservers();
+    }
+
+    public function test_subscribe_AddFirstSubscriber_Success() {
+        //given
+        //refer to Before Each method
+        $expectedAmountOfSubscribers = 15;
+        $followers = $this->generateObservers($expectedAmountOfSubscribers);
+
+        //when
+        $this->twitter->subscribe($followers);
+
+        //then
+        $this->assertCount($expectedAmountOfSubscribers, $this->twitter->getObservers());
+    }
+
+    public function test_subscribe_AddSubscribersToExistingList_Success(){
+        //given
+        //refer to Before Each method
+        $expectedAmountOfSubscriber = 30;
+        $followers = $this->generateObservers($expectedAmountOfSubscriber / 2);
+        $this->twitter->subscribe($followers);
+        $followersToAdd = $this->generateObservers($expectedAmountOfSubscriber / 2);
+
+        //when
+        $this->twitter->subscribe($followersToAdd);
+
+        //then
+        $this->assertCount($expectedAmountOfSubscriber, $this->twitter.getObservers());
+    }
+
+    public function test_subscribe_SubscriberAlreadyExists_ThrowsException()
+    {
+        //given
+        //refer to Before Each method
+        $expectedAmountOfSubscriber = 15;
+        $followers = $this->generateObservers($expectedAmountOfSubscriber);
+        $this->twitter->subscribe($followers);
+        $followersDuplicate = array();
+        $followersDuplicate->add($followers.get(0));
+
+        //when
+        //event is called directly by the assertion
+
+        //then
+        $this->expectException(SubscriberAlreadyExistsException::class);
+        $this->twitter->subscribe($followersDuplicate);
+    }
+
+    public function test_unsubscribe_NominalCase_Success()
+    {
+        //given
+        //refer to Before Each method
+        $expectedAmountOfSubscribers = 14;
+        $followers = $this->generateObservers($expectedAmountOfSubscribers + 1);
+        $this->twitter->subscribe($followers);
+
+        //when
+        $this->twitter->unsubscribe($followers.get(0));
+
+        //then
+        $this->assertCount($expectedAmountOfSubscribers, $this->twitter->getObservers());
+    }
+
+    public function test_unsubscribe_EmptyListOfSubscriber_ThrowsException()
+    {
+        //given
+        //refer to Before Each method
+        $followerToRemove = new Follower();
+
+        //when
+        //event is called directly by the assertion
+
+        //then
+        $this->expectException(EmptyListOfSubscribersException::class);
+        $this->twitter->unsubscribe($followerToRemove);
+    }
+
+    public function test_unsubscribe_SubscriberNotFound_ThrowsException()
+    {
+        //given
+        //refer to Before Each method
+        $followerNotFound = new Follower();
+        $this->twitter->subscribe(generateObserver(10));
+
+        //when
+        //event is called directly by the assertion
+
+        //then
+        $this->expectException(SubscriberNotFoundException::class);
+        $this->twitter->unsubscribe($followerNotFound);
     }
 
     //region private methods
-    private function generateObservers($amountOfObserversToCreate):array
+    #[Pure] private function generateObservers($amountOfObserversToCreate):array
     {
         $observers = array();
         for ($i=0 ; $i <= $amountOfObserversToCreate ; $i++)
@@ -57,6 +171,4 @@ class testTwitter extends TestCase
         return $observers;
     }
     //endregion private methods
-
-
 }
